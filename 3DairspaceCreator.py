9caa5cb46475660faff0d47a1d2c5379e6b64da0
bakadave@hotmail.com <bakadave@hotmail.com>
2020-09-20 23:01:38 +0200

    outerboundary as string (bad idea)

--- 3DairspaceCreator.py
+++ 3DairspaceCreator.py
@@ -5,35 +5,47 @@ import simplekml
 #lat = '472011N'
 #lon = '0181744E'
 
-airspaceName = 'BUDAPEST TMA1'
-line = '472011N 0181744E - 470220N 0182212E - 465337N 0190031E - 465726N 0185421E - 470324N 0184445E - 472011N 0181744E'
+airspaceName = 'BUDAPEST TMA4'
+line = '474111N 0185850E - 473556N 0185145E - 473355N 0185502E - 473057N 0185951E - 473721N 0190503E - 473827N 0190316E - 474111N 0185850E'
 topAlt = 'FL 195'
-bottomAlt = '9500 FT ALT'
+bottomAlt = '2500 FT ALT'
 clss = 'C'
 
 class Coordinate(NamedTuple):
     lon: int = []
     lat: int = []
 
-class Airspace(NamedTuple):
+class Airspace:
     name: str
     lon: int
     lat: int
     airspaceClass: str
     top: int
     bottom: int
+    numPoints: int
 
-    def outerboundary(lat, lon):
-        bdr = '('
-        idx = 0
-        for len(lat) in lon:
-            bdr += lat[idx]
+    def __init__(self, name, gps, airspaceClass, top, bottom):
+        self.name = name
+        self.lat = gps.lat
+        self.lon = gps.lon
+        self.clss = airspaceClass
+        self.bottom = bottom
+        self.top = top
+        self.numPoints = len(self.lon)
+
+    def outerboundary(self):
+        bdr = '['
+        for idx in range(self.numPoints):
+            bdr += '('
+            bdr += str(self.lon[idx])
             bdr += ','
-            bdr += lon[idx]
+            bdr += str(self.lat[idx])
             bdr += ',0'
             bdr += ')'
             bdr += ','
         bdr = bdr[:-1]
+        bdr += ']'
+        return bdr
 
 
 def splitCoordinates(string):
@@ -70,8 +82,9 @@ def dd2dms(deg):
 
 if __name__ == "__main__":
     kml = simplekml.Kml()
-    tma1 = Airspace(airspaceName, splitCoordinates(line).lat, splitCoordinates(line).lon, clss, altStr2Num(topAlt), altStr2Num(bottomAlt))
+    tma1 = Airspace(airspaceName, splitCoordinates(line), clss, altStr2Num(topAlt), altStr2Num(bottomAlt))
     #kml.newpoint(name=tma1.name, coords=[(tma1.lon[0], tma1.lat[0])])
-    #kml.newpolygon(name=tma1.name, outerboundaryis=[(tma1.lon, tma1.lat)])
+    print(tma1.outerboundary())
+    
+    #kml.newpolygon(name=tma1.name, outerboundaryis=[(18.585,47.4111,0),(18.514499999999998,47.3556,0),(18.5502,47.3355,0),(18.5951,47.3057,0),(19.0503,47.3721,0),(19.0316,47.3827,0),(18.585,47.4111,0)])
     #kml.save("test.kml")
-    print(tma1.outerboundary)
