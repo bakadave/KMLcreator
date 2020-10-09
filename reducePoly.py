077295ec7feea9277528c726a46c153f6f676c20
bakadave@hotmail.com <bakadave@hotmail.com>
2020-10-10 00:47:22 +0200

    RDP working

--- reducePoly.py
+++ reducePoly.py
@@ -1,6 +1,7 @@
 from rdp import rdp
 #import numpy as np
 from xml.dom import minidom
+from simplekml import Kml
 
 xmldoc = minidom.parse('imported maps/HUN_border_HIres.kml')
 itemlist = xmldoc.getElementsByTagName('coordinates')
@@ -17,12 +18,17 @@ for val in arr:
 print(len(poly))
 
 poly_red = []
-mask = rdp(poly, algo="iter", return_mask = True)
+mask = rdp(poly, algo="iter", return_mask = True, epsilon = 0.05)
 
 print(mask[0:10])
 
-# for idx, item in enumerate(poly):
-#     if mask[idx]:
-#         poly_red.append(poly[idx])
+for idx, item in enumerate(poly):
+    if mask[idx]:
+        poly_red.append(poly[idx])
 
-# print(len(poly_red))
+print(len(poly_red))
+
+kml = Kml(name="rdp")
+border = kml.newpolygon(name="RDP_border", outerboundaryis=poly_red)
+border.polystyle.color = '0000000f'
+kml.save("rdp.kml")
