ab69cccbab3ecbdef9a5d0b6473cbbdeae6e8ed0
bakadave@hotmail.com <bakadave@hotmail.com>
2020-10-25 00:12:36 +0200

    baisc parsing functionality

---
+++ notes.md
@@ -0,0 +1,16 @@
+# KMZ to KML converter
+https://mygeodata.cloud/converter/kmz-to-kml.Python+Tutorial+Read+data+from+text+file+and+split+into+multiple+filesIn
+
+# HTML scraping Tutorial
+https://www.dataquest.io/blog/web-scraping-beautifulsoup/
+
+# HTML scraping tutorial
+https://erikrood.com/Python_References/web_scrape.html
+
+# AIP
++ ENR 2.1: FIR, TMA, CTA, UIR
++ ENR 2.2: other regulated
++ ENR 5.5: glider airspaces
+
+# useful regex tool
+https://regexr.com/
--- parser.py
+++ parser.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
+import re
 #from urllib.request import urlopen
 
 url = "C:/Users/bakad/OneDrive/Desktop/AIRAC/2020-06-18-AIRAC/html/eAIP/LH-ENR-2.1-en-HU.html"
-#/html/body/div[2]/div/div/div[1]/table[3]/tbody/tr[1]
 
 with open(url) as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
-table = soup.find_all('table')[3]
-for row in table.findAll("tr"):
-    cells = row.findAll("td")
-    print(cells)
+ENR2_1 = [a.get_text() for a in soup.find_all('table')[3]]
+
+#https://regexr.com/ -> for much needed help
+#x = re.split(r"(BUDAPEST TMA\d?\/\w?|BUDAPEST TMA\d?)", TMA)
+TMA = ENR2_1[1].split("BUDAPEST TMA")
+TMA.pop(0)
+
+for idx, _ in enumerate(TMA):
+    TMA[idx] = "BUDAPEST TMA" + TMA[idx]
+
+TMAs = []
+for row in TMA:
+    print(row)
